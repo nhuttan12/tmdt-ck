b6630dfe842a1ce8f88d16ce3891fab7e87516f6
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-04-23 19:57:59 +0700

    create message file

---
+++ backend/src/helper/error-message.ts
@@ -0,0 +1,15 @@
+export const ErrorMessage = {
+  USER_NOT_FOUND: 'Tài khoản không tồn tại',
+  USER_ALREADY_EXISTS: 'Tài khoản đã tồn tại',
+  INVALID_PASSWORD: 'Mật khẩu không hợp lệ',
+  INVALID_EMAIL: 'Email không hợp lệ',
+  EMAIL_IS_NOT_EMPTY: 'Email không được để trống',
+  PASSWORD_IS_NOT_EMPTY: 'Mật khẩu không được để trống',
+  PASSWORD_MISMATCH: 'Mật khẩu không khớp',
+  USERNAME_IS_NOT_EMPTY: 'Tài khoản không được để trống',
+  NAME_IS_NOT_EMPTY: 'Tên người dùng không được để trống',
+  USER_NAME_HAVE_AT_LEAST_3_CHARACTERS:
+    'Tên người dùng phải có ít nhất 3 ký tự',
+  PASSWORD_HAVE_AT_LEAST_3_CHARACTERS: 'Mật khẩu phải có ít nhất 6 ký tự',
+  USERNAME_OR_EMAIL_EXISTS: 'Tài khoản hoặc email đã tồn tại',
+} as const;
---
+++ backend/src/helper/message-log.ts
@@ -0,0 +1,4 @@
+export const ErrorMessage = {
+  INVALID_TOKEN: 'Invalid token',
+  TOKEN_EXPIRED: 'Token expired',
+} as const;
