83675d99742394d560427b5dcef5f8dca381f1c3
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-04-23 20:05:13 +0700

    update user module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
 import configuration from './config/configuration';
-import { AppConfigService } from './config/app-config.service';
-import { DrizzleAsyncProvider } from './config/drizzle/drizzle.provider';
+import { AppConfigModule } from './config/app-config.module';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './user/user.module';
 
 @Module({
@@ -13,10 +13,9 @@ import { UsersModule } from './user/user.module';
       isGlobal: true,
     }),
     AuthModule,
+    AppConfigModule,
+    DatabaseModule,
     UsersModule,
   ],
-  controllers: [],
-  providers: [],
-  exports: [ConfigModule, AppConfigService, DrizzleAsyncProvider],
 })
 export class AppModule {}
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { eq } from 'drizzle-orm';
+import { MySql2Database } from 'drizzle-orm/mysql2';
+import { DrizzleAsyncProvider } from 'src/database/drizzle.provider';
+import { User } from 'src/db/helper/schema-type';
+import { users } from 'src/db/schema';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @Inject(DrizzleAsyncProvider)
+    private db: MySql2Database<User>,
+  ) {}
+
+  async getUserById(id: number): Promise<User> {
+    const [user] = await this.db.select().from(users).where(eq(users.id, id));
+    return user;
+  }
+}
