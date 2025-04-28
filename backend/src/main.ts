5ec0255d9e8a77203c8be47ea9a5ea817fd0b3fe
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-04-28 17:16:31 +0700

    add logger and get server port from yaml file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,21 +1,27 @@
 import { ConsoleLogger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: new ConsoleLogger({
       colors: true,
-      logLevels: ['log', 'fatal', 'error', 'warn', 'debug', 'verbose'],
+      logLevels: ['log', 'error', 'warn', 'debug', 'verbose'],
       timestamp: true,
     }),
   });
+
+  const configService: ConfigService = app.get(ConfigService);
+  const port: number = configService.get<number>('http.port') || 3000;
+
   app.useGlobalPipes(
     new ValidationPipe({
       enableDebugMessages: true,
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port);
+  console.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 bootstrap();
