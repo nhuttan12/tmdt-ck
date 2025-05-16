0d2a1263428df7b35e30f6164eb8d017374048db
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-05-16 20:49:57 +0700

    adding bearer auth for testing in swagger ui for user controller

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -4,79 +4,71 @@ import {
   HttpCode,
   HttpStatus,
   Param,
+  Put,
   Query,
   UseFilters,
   UseGuards,
 } from '@nestjs/common';
 import { User } from 'src/db/helper/schema-type';
 import { HasRole } from 'src/helper/decorator/roles.decorator';
+import {
+  FindUserById,
+  FindUserByName,
+} from 'src/helper/dto/user/find-user.dto';
+import { UserUpdateDTO } from 'src/helper/dto/user/update-user.dto';
 import { Role } from 'src/helper/enum/role.enum';
 import { CatchEverythingFilter } from 'src/helper/filter/exception.filter';
 import { JwtAuthGuard } from 'src/helper/guard/jwt-auth.guard';
-import { LocalAuthGuard } from 'src/helper/guard/local-ath.guard';
 import { GetAllUsersDto } from '../../helper/dto/user/get-all-user.dto';
 import { UserService } from './user.service';
-import { FindUserById, FindUserByName } from 'src/helper/dto/user/find-user.dto';
-import { UserUpdateDTO } from 'src/helper/dto/user/update-user.dto';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { RolesGuard } from 'src/helper/guard/roles.guard';
 
 @Controller('v2/users')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get()
+  @ApiBearerAuth('jwt')
   @HttpCode(HttpStatus.OK)
-  @UseGuards(LocalAuthGuard)
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @HasRole(Role.ADMIN)
   @UseFilters(CatchEverythingFilter)
-  async getAllUsers(
-    @Param() params: GetAllUsersDto,
-    @Query() query: GetAllUsersDto,
-  ) {
-    const page: number = params.page ?? query.page ?? 1;
-    const limit: number = params.limit ?? query.limit ?? 10;
+  async getAllUsers(@Query() query: GetAllUsersDto) {
+    const { page, limit }: GetAllUsersDto = query;
     return this.userService.getAllUsers(limit, page);
   }
 
-  @Get()
+  @Get(':id')
+  @ApiBearerAuth('jwt')
   @HttpCode(HttpStatus.OK)
-  @UseGuards(JwtAuthGuard)
-  @UseGuards(LocalAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @HasRole(Role.ADMIN)
   @UseFilters(CatchEverythingFilter)
-  async findUserById(
-    @Param() idParam: FindUserById,
-    @Query() idQuery: FindUserById,
-  ): Promise<User[]> {
-    const id: number = idParam.id ?? idQuery.id;
+  async findUserById(@Param() idParam: FindUserById): Promise<User[]> {
+    const id: number = idParam.id;
     return this.userService.findUserById(id);
   }
 
-  @Get()
+  @Get(':name')
+  @ApiBearerAuth('jwt')
   @HttpCode(HttpStatus.OK)
-  @UseGuards(JwtAuthGuard)
-  @UseGuards(LocalAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @HasRole(Role.ADMIN)
   @UseFilters(CatchEverythingFilter)
-  async findUserByName(
-    @Param() nameParam: FindUserByName,
-    @Query() nameQuery: FindUserByName,
-  ): Promise<User[]> {
-    const name: string = nameParam.name ?? nameQuery.name;
+  async findUserByName(@Param() nameParam: FindUserByName): Promise<User[]> {
+    const name: string = nameParam.name;
     return this.userService.findUserByName(name);
   }
 
-  @Get()
+  @Put()
+  @ApiBearerAuth('jwt')
   @HttpCode(HttpStatus.OK)
-  @UseGuards(LocalAuthGuard)
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @HasRole(Role.ADMIN)
   @UseFilters(CatchEverythingFilter)
-  async updateUser(
-    @Param() userParam: UserUpdateDTO,
-    @Query() userQuery: UserUpdateDTO,
-  ): Promise<void> {
-    const user: UserUpdateDTO = userParam ?? userQuery;
+  async updateUser(@Query() userQuery: UserUpdateDTO): Promise<void> {
+    const user: UserUpdateDTO = userQuery;
     return this.userService.updateUser(user);
   }
 }
