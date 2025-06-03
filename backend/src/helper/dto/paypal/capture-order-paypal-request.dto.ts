cdcac436b2bd4233f393e61942d5492a50ec8ac6
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-06-03 15:42:44 +0700

    update dto

--- backend/src/helper/dto/paypal/capture-order-paypal-request.dto.ts
+++ backend/src/helper/dto/paypal/capture-order-paypal-request.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsNotEmpty, Min } from 'class-validator';
 import { OrderErrorMessage } from 'src/helper/message/order-error-message';
 
@@ -5,5 +6,6 @@ export class CaptureOrderDto {
   @IsNotEmpty({ message: OrderErrorMessage.ORDER_ID_SHOULD_NOT_BE_EMPTY })
   @IsInt({ message: OrderErrorMessage.ID_MUST_BE_INTEGER })
   @Min(1, { message: OrderErrorMessage.ORDER_ID_MUST_BE_A_POSITIVE_NUMBER })
+  @ApiProperty()
   orderId: number;
 }
